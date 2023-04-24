ffd68d59cf84f65c4d7a23b2d7adf8fc4214b98b
Aviv Levy <avvvviv@walla.co.il>
2023-04-24 21:22:13 +0300

    Project Doneee

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -5,9 +5,15 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const fs = require('fs')
+const bcrypt = require('bcrypt')
 const cors = require('cors');
 
 app.use(cors())
+
+const CustomerModel = require('./Models/customerModel')
+const ContactModel = require('./Models/contactModel')
+const UserModel = require('./Models/userModel')
 
 const loginRouter = require('./Routers/loginRouter.js')
 const getDetailsRouter = require('./Routers/getDetailsRouter.js')
@@ -35,6 +41,35 @@ app.use('/login', loginRouter)
 app.use('/customers', customerRouter)
 app.use('/contacts', contactRouter)
 app.use('/getDetails', getDetailsRouter)
+
+// http://localhost:3000/initilaize
+app.get('/initilaize', async (req, res) => {
+    try {
+        let myData = JSON.parse(fs.readFileSync("./initalizeData.json").toString());
+
+        // Insert users
+        myData.users.forEach(async (user) => {
+            user.password = await bcrypt.hash(user.password, 10)
+            const newUser = new UserModel(user);
+            await newUser.save();
+        });
+
+        // Insert customers
+        myData.customers.forEach(async (customer) => {
+            const newCustomer = new CustomerModel(customer);
+            await newCustomer.save();
+        });
+        
+        // Insert contacts
+        myData.contacts.forEach(async (contact) => {
+            const newContact = new ContactModel(contact);
+            await newContact.save();
+        });
+
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+})
 
 app.listen(port, () => {
     console.log('Server is running...');
---
+++ README.md
@@ -0,0 +1,18 @@
+# CompanyCRM
+
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 15.2.0.
+
+## Development server
+
+Run `node app.js` for a Nodejs server. Navigate to `http://localhost:3000/initilaize` to initilaize data in monogoDB.
+
+Then Run `ng serve` for angular dev server. Navigate to `http://localhost:4200` to see the site.
+
+## Login
+
+After running the Initialization you can login with the user:
+
+`Username: tester@gmail.com`
+
+`Password: 123456`
+
